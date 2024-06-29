3daf87cfb9240430e1c8e4c7637e3e62886acf00
Will Purvis <williampurvis00@gmail.com>
2024-06-29 12:40:55 +0100

    renamed alfi files for clarity

--- src/gpytorch_alfi/notebook.py
+++ src/gpytorch_alfi/notebook.py
@@ -11,10 +11,10 @@
 import torch
 import numpy as np
 
-from dataset import PyTorchDataset
-from model import ExactLFM
-from trainer import TorchTrainer
-from plotter import plot_lf, plot_gxpred, plot_comparison_torch
+from dataset_alfi import PyTorchDataset
+from model_alfi import ExactLFM
+from trainer_alfi import TorchTrainer
+from plotter_alfi import plot_lf, plot_gxpred, plot_comparison_torch
 
 from gpytorch.mlls.exact_marginal_log_likelihood import ExactMarginalLogLikelihood
 
