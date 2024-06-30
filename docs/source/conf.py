283b40d8f0a7173332d5875a94b8811e3fa3b4f6
Will Purvis <williampurvis00@gmail.com>
2024-06-30 15:08:54 +0100

    removed static js for html docs build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,7 +41,6 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "latent_forces/"]
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "pydata_sphinx_theme"
-html_static_path = ["_static"]
 
 
 # TODO: Split documentation into two parts:
